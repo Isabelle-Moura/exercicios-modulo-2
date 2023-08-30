6998116cab0ea1b4e3829c2eaaa0de916e4374ca
Isabelle-Moura <mourabisabelle@gmail.com>
2023-08-30 14:58:10 -0300

    2023-08-30

--- aula 10/src/App.tsx
+++ aula 10/src/App.tsx
@@ -35,89 +35,151 @@ Crie um novo tipo ProductForm que seja uma versão modificada da interface Produ
 Exercício 4: 
 Gere um projeto Typescript/React utilizando o Vite. No seu app.tsx, crie um tipo chamado UserData. Este tipo deverá conter nome, idade e gênero. Crie um estado que seja deste tipo (lembre-se dos genéricos) e faça um formulário para preencher este estado.
 
+//////////////
+
 */
-import UserList from "./components/Users/UserList";
-import CarList from "./components/Cars/CarList";
-import ProductList from "./components/Products/ProductList";
+// import UserList from "./components/Users/UserList";
+// import CarList from "./components/Cars/CarList";
+// import ProductList from "./components/Products/ProductList";
 
-const users: User[] = [
-  {
-    id: 1,
-    name: "Isabelle",
-    age: 19,
-    email: "mourabisabelle@gmail.com",
-  } as User,
-  {
-    id: 2,
-    name: "Yllana",
-    age: 21,
-    email: "moura.yllana@gmail.com",
-  } as User,
-  {
-    id: 3,
-    name: "Jéssica",
-    age: 24,
-    email: "jessie.moura19@gmail.com",
-  } as User,
-  {
-    id: 4,
-    name: "Jorge",
-    age: 29,
-  } as User,
-];
+// const users: User[] = [
+//   {
+//     id: 1,
+//     name: "Isabelle",
+//     age: 19,
+//     email: "mourabisabelle@gmail.com",
+//   } as User,
+//   {
+//     id: 2,
+//     name: "Yllana",
+//     age: 21,
+//     email: "moura.yllana@gmail.com",
+//   } as User,
+//   {
+//     id: 3,
+//     name: "Jéssica",
+//     age: 24,
+//     email: "jessie.moura19@gmail.com",
+//   } as User,
+//   {
+//     id: 4,
+//     name: "Jorge",
+//     age: 29,
+//   } as User,
+// ];
 
-const cars: CarSummary[] = [
-  {
-    brand: "Citroën",
-    model: "Aircross",
-    year: "2010",
-  },
-  {
-    brand: "Gurgel",
-    model: "Bugato",
-    year: "1970",
-  },
-  {
-    brand: "Simca",
-    model: "Esplanada",
-    year: "1967",
-  },
-];
+// const cars: CarSummary[] = [
+//   {
+//     brand: "Citroën",
+//     model: "Aircross",
+//     year: "2010",
+//   },
+//   {
+//     brand: "Gurgel",
+//     model: "Bugato",
+//     year: "1970",
+//   },
+//   {
+//     brand: "Simca",
+//     model: "Esplanada",
+//     year: "1967",
+//   },
+// ];
 
-const products: ProductForm[] = [
-  {
-    name: "Product 1",
-    description: "Description of Product 1",
-    category: "Category A",
-    quantity: 10,
-  },
-  {
-    name: "Product 2",
-    description: "Description of Product 2",
-    category: "Category B",
-  },
-  {
-    name: "Product 3",
-    description: "Description of Product 3",
-    category: "Category A",
-    quantity: 5,
-  },
-];
+// const products: ProductForm[] = [
+//   {
+//     name: "Product 1",
+//     description: "Description of Product 1",
+//     category: "Category A",
+//     quantity: 10,
+//   },
+//   {
+//     name: "Product 2",
+//     description: "Description of Product 2",
+//     category: "Category B",
+//   },
+//   {
+//     name: "Product 3",
+//     description: "Description of Product 3",
+//     category: "Category A",
+//     quantity: 5,
+//   },
+// ];
+
+// 👉 Isso daqui tem que colocar dentro do componente App
+{
+  /* <div>
+  <UserList users={users} />
+</div>
+<hr />
+<div>
+  <CarList cars={cars} />
+</div>
+<hr />
+<div>
+  <ProductList products={products} />
+</div> */
+}
+
+import { useState } from "react";
+import Label from "./components/Form/Label";
+import Input from "./components/Form/Input";
+
+interface UserData {
+  name: string;
+  age: number;
+  gender: string;
+}
 
 const App = () => {
+  const [userData, setUserData] = useState<UserData>({
+    name: "",
+    age: 0,
+    gender: "",
+  });
+
+  const handleInputChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ): void => {
+    const { name, value } = event.target;
+    setUserData((prevUserData) => ({
+      ...prevUserData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+    console.log("User data: " + userData);
+  };
+
   return (
     <>
-      <div>
-        <UserList users={users} />
-      </div>
-      <hr />
-      <div>
-        <CarList cars={cars} />
-      </div>
-      <hr />
-      <div>
-        <ProductList products={products} />
-      </div>
+      <h1>Formulário</h1>
+      <form onSubmit={handleSubmit}>
+        <Label title="Nome:" />
+        <Input
+          name="name"
+          placeholder="Digite o seu nome."
+          onChange={handleInputChange}
+        />
+        <br />
+        <Label title="Idade:" />
+        <Input
+          name="age"
+          placeholder="Digite a sua idade."
+          onChange={handleInputChange}
+        />
+        <br />
+        <Label title="Gênero:" />
+        <Input
+          name="gender"
+          placeholder="Digite o seu gênero."
+          onChange={handleInputChange}
+        />
+        <br />
+        <button type="submit">Enviar</button>
+      </form>
     </>
   );
 };
---
+++ aula 10/src/components/Form/Input.tsx
@@ -0,0 +1,34 @@
+import { styled } from "styled-components";
+
+interface InputType {
+  placeholder: string;
+  name: string;
+  type?: "text";
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const InputStyle = styled.input`
+  width: 200px;
+  height: 40px;
+  font-size: 20px;
+  padding: 0px 10px;
+  border: 1px solid indigo;
+  border-radius: 10px;
+  font-weight: 800;
+  font-family: "Poppins", sans-serif;
+`;
+
+const Input = ({ placeholder, name, type, onChange }: InputType) => {
+  return (
+    <>
+      <InputStyle
+        placeholder={placeholder}
+        name={name}
+        type={type}
+        onChange={onChange}
+      />
+    </>
+  );
+};
+
+export default Input;
